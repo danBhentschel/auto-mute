a8265ce53fe83fce6b762a9e7021ce4d8add8578
Dan Hentschel <dan@truepic.com>
2025-03-28 06:30:36 -0400

    Always send color scheme from offscreen document.

--- extension/offscreen.js
+++ extension/offscreen.js
@@ -1,8 +1,5 @@
 (function (_chrome, _matchMedia, _setInterval, _console) {
-  let last_scheme = "unset";
-
   async function send_scheme(scheme) {
-    last_scheme = scheme;
     _console.log(`Sending system color scheme: ${scheme}`);
     try {
       const response = await _chrome.runtime.sendMessage({
@@ -10,7 +7,6 @@
         data: { scheme },
       });
       _console.log(`Color scheme change response: ${JSON.stringify(response)}`);
-      last_scheme = response.systemColorScheme;
     } catch (e) {
       _console.error(e);
     }
@@ -27,8 +23,6 @@
   _setInterval(async () => {
     const dark_mode_query = _matchMedia("(prefers-color-scheme: dark)");
     const current_scheme = dark_mode_query.matches ? "dark" : "light";
-    if (current_scheme !== last_scheme) {
-      await send_scheme(current_scheme);
-    }
+    await send_scheme(current_scheme);
   }, 1000);
 })(window.chrome, window.matchMedia, window.setInterval, window.console);
