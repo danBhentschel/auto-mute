1b250ba1f16a236e6ed3602d4f8d4e98ede357b1
Dan Hentschel <dan@truepic.com>
2025-03-28 06:58:00 -0400

    Use session storage to cache the system color scheme.

--- extension/IconSwitcher.js
+++ extension/IconSwitcher.js
@@ -5,8 +5,8 @@ class IconSwitcher {
   #tabTracker;
   /** @member {Object} */
   #logger;
-  /** @member {'dark' | 'light'} */
-  #systemColorScheme;
+  /** @member {'dark' | 'light' | 'unset'} */
+  #systemColorScheme = "unset";
 
   /**
    * @param {Object} chromeInstance
@@ -17,7 +17,6 @@ class IconSwitcher {
     this.#chrome = chromeInstance;
     this.#tabTracker = tabTracker;
     this.#logger = logger;
-    this.#systemColorScheme = "unset";
   }
 
   /**
@@ -40,27 +39,59 @@ class IconSwitcher {
    */
   async setSystemColorScheme(scheme) {
     this.#logger.log(`System color scheme changed to ${scheme}`);
-    this.#systemColorScheme = scheme;
+    await this.#setSystemColorScheme(scheme);
     await this.updateIcon();
     return scheme;
+  }
+
+  /**
+   * @param {'dark' | 'light'} scheme
+   * @returns {Promise<void>}
+   */
+  async #setSystemColorScheme(scheme) {
+    // We need to set the system color scheme in the storage to persist it
+    // because the service worker is not always running, so in-memory state
+    // will sometimes be lost.
+    await this.#chrome.storage.session.set({
+      systemColorScheme: scheme,
+    });
+    this.#systemColorScheme = scheme;
+  }
+
+  /**
+   * @returns {Promise<'dark' | 'light' | 'unset'>}
+   */
+  async #getSystemColorScheme() {
+    // If the system color scheme is unset, we need to fetch it from storage
+    // because the service worker is not always running, so in-memory state
+    // will sometimes be lost.
+    if (this.#systemColorScheme === "unset") {
+      this.#logger.log("Fetching system color scheme from storage");
+      this.#systemColorScheme = await this.#chrome.storage.session.get({
+        systemColorScheme: "unset",
+      }).systemColorScheme;
+    }
+    return this.#systemColorScheme;
   }
 
   /**
    * @returns {Promise<void>}
    */
   async updateIcon() {
-    if (this.#systemColorScheme === "unset") {
+    let systemColorScheme = await this.#getSystemColorScheme();
+
+    if (systemColorScheme === "unset") {
       this.#logger.log(
-        "Not updating icon because system color scheme is unset"
+        "System color scheme is unset. Assuming light mode for icon."
       );
-      return;
+      systemColorScheme = "light";
     }
 
     const onOrOff = (await this.#tabTracker.isCurrentTabMutedByExtension())
       ? "off"
       : "on";
 
-    const path = `images/${this.#systemColorScheme}_${onOrOff}_16.png`;
+    const path = `images/${systemColorScheme}_${onOrOff}_16.png`;
     this.#logger.log(`Setting icon to ${path}`);
     await this.#chrome.action.setIcon({
       path,
--- extension/offscreen.js
+++ extension/offscreen.js
@@ -1,4 +1,6 @@
 (function (_chrome, _matchMedia, _setInterval, _console) {
+  let last_scheme = "unset";
+
   async function send_scheme(scheme) {
     _console.log(`Sending system color scheme: ${scheme}`);
     try {
@@ -7,7 +9,9 @@
         data: { scheme },
       });
       _console.log(`Color scheme change response: ${JSON.stringify(response)}`);
+      last_scheme = response.systemColorScheme;
     } catch (e) {
+      last_scheme = "unset";
       _console.error(e);
     }
   }
@@ -23,6 +27,8 @@
   _setInterval(async () => {
     const dark_mode_query = _matchMedia("(prefers-color-scheme: dark)");
     const current_scheme = dark_mode_query.matches ? "dark" : "light";
-    await send_scheme(current_scheme);
+    if (current_scheme !== last_scheme) {
+      await send_scheme(current_scheme);
+    }
   }, 1000);
 })(window.chrome, window.matchMedia, window.setInterval, window.console);
